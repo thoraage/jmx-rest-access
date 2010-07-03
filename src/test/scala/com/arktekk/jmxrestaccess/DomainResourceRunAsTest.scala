aafa6257efd674ab30531bb23630c20abe9750a9
thoraage <thoraageeldby@gmail.com>
2010-07-03 19:40:26 +0200

    * Simplified the test a tiny bit

--- src/test/scala/com/arktekk/jmxrestaccess/DomainResourceRunAsTest.scala
+++ src/test/scala/com/arktekk/jmxrestaccess/DomainResourceRunAsTest.scala
@@ -3,7 +3,6 @@ package com.arktekk.jmxrestaccess
 import org.junit.runner.RunWith
 import org.specs.runner.{JUnit4, JUnitSuiteRunner}
 import org.specs.Specification
-import javax.servlet.http.HttpServletRequest
 import javax.management.MBeanServerConnection
 import org.specs.mock.Mockito
 import xml.NodeSeq
@@ -14,20 +13,22 @@ import xml.NodeSeq
 @RunWith(classOf[JUnitSuiteRunner])
 class DomainResourceRunAsTest extends JUnit4(DomainResourceSpec)
 object DomainResourceSpec extends Specification with Mockito {
+  val host = "anyhost:8080"
+
   val domainResource = new DomainResource {
-    def jmxHelper = new JMXHelper {
-      def getMBeanServerConnection(request: HttpServletRequest, host: String): MBeanServerConnection = {
-        val mBeanServerConnection = mock[MBeanServerConnection]
-        doReturn(Array("Domain1").asInstanceOf[scala.runtime.BoxedAnyArray].unbox(classOf[String])).when(mBeanServerConnection).getDomains()
-        mBeanServerConnection
-      }
+    def jmxHelper = {
+      val helper = mock[JMXHelper]
+      val connection = mock[MBeanServerConnection]
+      doReturn(Array("Domain1").asInstanceOf[scala.runtime.BoxedAnyArray].unbox(classOf[String])).when(connection).getDomains()
+      doReturn(connection).when(helper).getMBeanServerConnection(null, host)
+      helper
     }
   }
 
   "root resource" should {
 
     "return list of resources" in {
-      val listElements: NodeSeq = domainResource.getAll(TestHelper.getUriInfo, null, "anyhost:8080") \\ "html" \\ "body" \\ "ul" \\ "li"
+      val listElements: NodeSeq = domainResource.getAll(TestHelper.getUriInfo, null, host) \\ "html" \\ "body" \\ "ul" \\ "li"
       listElements(0).text.trim must_== "Domain1"
       (listElements(0) \\ "@href").text must_== "http://arktekk.no/rest/anyhost:8080/mbeans?domainName=Domain1"
     }
